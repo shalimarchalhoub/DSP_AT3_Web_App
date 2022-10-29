501a77b773bb201b6a540e13878c8fb8de8c1d07
Shalimar <shalimarchalhoub@hotmail.com>
2022-10-29 11:38:41 +1100

    First commit

---
+++ src/config.py
@@ -0,0 +1,116 @@
+import streamlit as st
+
+def set_app_config():
+    """
+    --------------------
+    Description
+    --------------------
+    -> set_app_config (function): Function that sets the configuration of the Streamlit app
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    => To be filled by student
+
+def set_session_state(key, value):
+    """
+    --------------------
+    Description
+    --------------------
+    -> set_session_state (function): Function that saves a key-value pair to the Streamlit session state
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    => To be filled by student
+
+def set_session_states(keys, value=None):
+    """
+    --------------------
+    Description
+    --------------------
+    -> set_session_states (function): Function that saves a list of key-value pairs to the Streamlit session state using set_session_state() (default value: None)
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    => To be filled by student
+
+def display_session_state():
+    """
+    --------------------
+    Description
+    --------------------
+    -> display_session_state (function): Function that displays the current values of Streamlit session state
+
+    --------------------
+    Parameters
+    --------------------
+    => To be filled by student
+    -> name (type): description
+
+    --------------------
+    Pseudo-Code
+    --------------------
+    => To be filled by student
+    -> pseudo-code
+
+    --------------------
+    Returns
+    --------------------
+    => To be filled by student
+    -> (type): description
+
+    """
+    => To be filled by student
+
+
+
