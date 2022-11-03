e04d990a15f890fb3ff614fccd698f081da67bc0
PhoebeJacinda <62164605+PhoebeJacinda@users.noreply.github.com>
2022-11-03 23:31:58 +1100

    Update logics.py

--- src/dataframe/logics.py
+++ src/dataframe/logics.py
@@ -28,14 +28,28 @@ class Dataset:
     -> date_cols (list): List of columns of datetime type (optional)
     """
     def __init__(self, schema_name=None, table_name=None, db=None, df=None):
-        => To be filled by student
+        #=> To be filled by student
+        self.schema_name = schema_name
+        self.table_name = table_name
+        self.db = db
+        sqlquery = "select * from " + self.table_name
+        self.df = pd.read_sql_query(sqlquery,db.conn)
+        self.n_rows = 0
+        self.n_cols = 0
+        self.n_duplicates = 0
+        self.n_missing = 0
+        self.num_cols = []
+        self.text_cols = []
+        self.date_cols = []
 
     def set_data(self):
         """
         --------------------
         Description
         --------------------
-        -> set_data (method): Class method that computes all requested information from self.df to be displayed in the Overall section of Streamlit app 
+        -> set_data (method): Class method that computes 
+        all requested information from self.df 
+        to be displayed in the Overall section of Streamlit app 
 
         --------------------
         Parameters
@@ -56,7 +70,16 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        # => To be filled by student
+        #if self.is_df_none() == False:
+        self.set_dimensions()
+        st.write("sudah")
+        self.set_duplicates()
+        self.set_missing()
+        self.set_numeric_columns()
+        self.set_date_columns()
+        self.set_text_columns()
+        #st.session_state['data'] = self
         
     def is_df_none(self):
         """
@@ -84,7 +107,8 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        return self.df.empty 
 
     def set_dimensions(self):
         """
@@ -112,7 +136,9 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        self.n_rows = self.df.shape[0]
+        self.n_cols = self.df.shape[1]
 
     def set_duplicates(self):
         """
@@ -140,7 +166,8 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        self.n_duplicates = self.df[self.df.duplicated()]
 
     def set_missing(self):
         """
@@ -168,15 +195,19 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        self.n_missing = self.df.isnull().sum().sum()
 
     def set_numeric_columns(self):
         """
         --------------------
         Description
         --------------------
-        -> set_numeric_columns (method): Class method that extract the list of numeric columns from a table using a SQL query (from get_numeric_tables_query()),
-        store it as attribute (self.num_cols) and then convert the relevant columns of self.df accordingly.
+        -> set_numeric_columns (method): Class method that 
+        extract the list of numeric columns from a table using a SQL query 
+        (from get_numeric_tables_query()),
+        store it as attribute (self.num_cols) and 
+        then convert the relevant columns of self.df accordingly.
 
         --------------------
         Parameters
@@ -197,15 +228,22 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        query = get_numeric_tables_query(self.schema_name, self.table_name)
+        self.df = self.db.run_query(query)
+        #print(self.df)
+        self.num_cols = self.df["table_name"].values.tolist()
 
     def set_text_columns(self):
         """
         --------------------
         Description
         --------------------
-        -> set_text_columns (method): Class method that extract the list of text columns from a table using a SQL query (from get_numeric_tables_query()),
-        store it as attribute (self.text_cols) and then convert the relevant columns of self.df accordingly.
+        -> set_text_columns (method): Class method that extract the 
+        list of text columns from a table using a SQL query 
+        (from get_numeric_tables_query()),
+        store it as attribute (self.text_cols) and 
+        then convert the relevant columns of self.df accordingly.
 
         --------------------
         Parameters
@@ -226,7 +264,11 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        query = get_text_tables_query(self.schema_name, self.table_name)
+        self.df = self.db.run_query(query)
+        #print(self.df)
+        self.text_cols = self.df["table_name"].values.tolist()
 
     def set_date_columns(self):
         """
@@ -255,7 +297,10 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        query = get_date_tables_query(self.schema_name, self.table_name)
+        self.df = self.db.run_query(query)
+        self.date_cols  = self.df["table_name"].values.tolist()
 
     def get_head(self, n=5):
         """
@@ -283,7 +328,8 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        return self.df.head(n)
 
     def get_tail(self, n=5):
         """
@@ -311,7 +357,8 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        return self.df.tail(n)
 
     def get_sample(self, n=5):
         """
@@ -339,14 +386,17 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        return self.df.sample(n)
 
     def get_summary_df(self):
         """
         --------------------
         Description
         --------------------
-        -> get_summary_df (method): Class method that formats all requested information from self.df to be displayed in the Overall section of Streamlit app as a Pandas dataframe with 2 columns: Description and Value
+        -> get_summary_df (method): Class method that formats 
+        all requested information from self.df to be displayed 
+        in the Overall section of Streamlit app as 
+        a Pandas dataframe with 2 columns: Description and Value
 
         --------------------
         Parameters
@@ -367,4 +417,17 @@ class Dataset:
         -> (type): description
 
         """
-        => To be filled by student
+        #=> To be filled by student
+        self.df = pd.DataFrame({'Description':['Table Name',
+                                        'Number of Rowss',
+                                        'Number of Columns',
+                                        'Number of Duplicated Rows',
+                                        'Number of Rows with Missing Values'
+                                        ],
+                    'Values':[str(self.table_name),
+                                str(self.n_rows),
+                                str(self.n_cols),
+                                str(self.n_duplicates),
+                                str(self.n_missing),
+                                ]})
+        return self.df
