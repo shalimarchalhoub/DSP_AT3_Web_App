a591a090f9f4ac4623fdad07a98779560d799f54
MAHJABEENMOHIUDDIN <113221570+MAHJABEENMOHIUDDIN@users.noreply.github.com>
2022-11-08 00:58:09 +1100

    Add files via upload
    commit

--- src/config.py
+++ src/config.py
@@ -1,116 +1,19 @@
 import streamlit as st
-
+from PIL import Image
 def set_app_config():
-    """
-    --------------------
-    Description
-    --------------------
-    -> set_app_config (function): Function that sets the configuration of the Streamlit app
 
-    --------------------
-    Parameters
-    --------------------
-    => To be filled by student
-    -> name (type): description
+    icon = Image.open('/home/mahjabeen/analysis.png')
+    st.set_page_config(
+    page_title="Streamlit application for performing data exploration on a database",
+    page_icon=icon,
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+            'Get Help': 'https://streamlit.io',
+            'Report a bug': 'https://github.com',
+            'About':'About your applicatioin: **data exploration on a database***'
+            }
+    )
 
-    --------------------
-    Pseudo-Code
-    --------------------
-    => To be filled by student
-    -> pseudo-code
-
-    --------------------
-    Returns
-    --------------------
-    => To be filled by student
-    -> (type): description
-
-    """
-    => To be filled by student
 
 def set_session_state(key, value):
-    """
-    --------------------
-    Description
-    --------------------
-    -> set_session_state (function): Function that saves a key-value pair to the Streamlit session state
-
-    --------------------
-    Parameters
-    --------------------
-    => To be filled by student
-    -> name (type): description
-
-    --------------------
-    Pseudo-Code
-    --------------------
-    => To be filled by student
-    -> pseudo-code
-
-    --------------------
-    Returns
-    --------------------
-    => To be filled by student
-    -> (type): description
-
-    """
-    => To be filled by student
-
-def set_session_states(keys, value=None):
-    """
-    --------------------
-    Description
-    --------------------
-    -> set_session_states (function): Function that saves a list of key-value pairs to the Streamlit session state using set_session_state() (default value: None)
-
-    --------------------
-    Parameters
-    --------------------
-    => To be filled by student
-    -> name (type): description
-
-    --------------------
-    Pseudo-Code
-    --------------------
-    => To be filled by student
-    -> pseudo-code
-
-    --------------------
-    Returns
-    --------------------
-    => To be filled by student
-    -> (type): description
-
-    """
-    => To be filled by student
-
-def display_session_state():
-    """
-    --------------------
-    Description
-    --------------------
-    -> display_session_state (function): Function that displays the current values of Streamlit session state
-
-    --------------------
-    Parameters
-    --------------------
-    => To be filled by student
-    -> name (type): description
-
-    --------------------
-    Pseudo-Code
-    --------------------
-    => To be filled by student
-    -> pseudo-code
-
-    --------------------
-    Returns
-    --------------------
-    => To be filled by student
-    -> (type): description
-
-    """
-    => To be filled by student
-
-
-
