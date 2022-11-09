2fa12ebd648bf9e3852e6f94c12b60ba29b0e9a1
shalimarchalhoub <114060299+shalimarchalhoub@users.noreply.github.com>
2022-11-09 19:01:43 +1100

    Update test_serie_text_logics.py

--- src/test/test_serie_text_logics.py
+++ src/test/test_serie_text_logics.py
@@ -1,9 +1,51 @@
 import unittest
 import pandas as pd
 
-from src.serie_text.logics import TextColumn
+from src.serie_text.queries import *
 
-=> To be filled by student
+class TestGetMissingQuery(unittest.TestCase):
+    def test_get_missing_query(self):
+        schema_name = "public"
+        table_name = "supplier"
+        col_name = "column"
+        query = ' '.join(get_missing_query(schema_name,table_name,col_name).split())
+        self.assertEqual(query,' '.join("""
+        
+        SELECT COUNT(test_col) 
+        FROM public.test_table 
+        WHERE test_col IS NULL;
+        """.split()))
+
+class TestGetModeQuery(unittest.TestCase):
+    def test_get_mode_query(self):
+        schema_name = "public"
+        table_name = "supplier"
+        col_name = "column"
+        query = ' '.join(get_mode_query(schema_name,table_name,col_name).split())
+        self.assertEqual(query, ' '.join("""
+
+        SELECT MODE()
+        WITHIN GROUP(ORDER BY test_col) AS mode
+        FROM public.test_table;
+        
+        """.split()))
+
+class TestGetAlphaQuery(unittest.TestCase):
+    def test_get_alpha_query(self):
+        schema_name = "public"
+        table_name = "supplier"
+        col_name = "column"
+        query = ' '.join(get_alpha_query(schema_name, table_name, col_name).split())
+        self.assertEqual(query, ' '.join("""
+        
+        SELECT COUNT(test_col) 
+        FROM public.test_table
+        WHERE test_col LIKE '%[a-zA-Z]%';
+        
+        
+        
+        """.split()))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
